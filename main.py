7a29b65fff373779ba177100d7991e214e275530
Travis Dawson <t.dawson@student.maastrichtuniversity.nl>
2023-07-28 09:46:58 +0200

    Basic Tkinter working

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import tkinter as tk
+from tkinter import messagebox
 
 if __name__ == "__main__":
-    print("Hello world")
+    root = tk.Tk()  # Main canvas
+    root.title("Simple Pop-up GUI")  # Labelling the main canvas
+
+    popup_button = tk.Button(root, text="Show Pop-up", command=messagebox.showinfo("Pop-up", "Pop-up checking"))  # Click button to activate pop-up
+    popup_button.pack(pady=20)
+
+    root.mainloop()
+
+
 
