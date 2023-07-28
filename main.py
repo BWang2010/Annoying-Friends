c309f183e74e7f78ac07f446b50497f46835ebd0
Travis Dawson <t.dawson@student.maastrichtuniversity.nl>
2023-07-28 09:51:23 +0200

    Only pop up box is now showing

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import tkinter as tk
 from tkinter import messagebox
 
+
 if __name__ == "__main__":
     root = tk.Tk()  # Main canvas
     root.title("Simple Pop-up GUI")  # Labelling the main canvas
+    root.withdraw()  # Hide the main canvas
 
-    popup_button = tk.Button(root, text="Show Pop-up", command=messagebox.showinfo("Pop-up", "Pop-up checking"))  # Click button to activate pop-up
-    popup_button.pack(pady=20)
+    messagebox.showinfo("Pop-up", "Click the red x to close")
 
     root.mainloop()
 
