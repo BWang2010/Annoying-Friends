224d3e3ab6903f6f20cc13d0a24ea2d0ea4c75c8
BWang2010 <bella.y.zhi@gmail.com>
2023-08-29 08:45:48 +0100

    getting red x to move randomly

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 import tkinter as tk
+import random
 
 
-def show_custom_popup():
-    popup = tk.Toplevel(root)
-    popup.title("Custom Pop-up")
 
-    # Set the position of the pop-up window
-    popup.geometry("400x400+2000+200")  # Width x Height + X_Offset + Y_Offset
 
-    label = tk.Label(popup, text="Click the red x to close")
-    label.pack(pady=20)
 
-    popup.mainloop()
+
+
+def callback(e):
+    x= e.x
+    y= e.y
+    print("Pointer is currently at %d, %d" %(x,y))
+
+def update_position():
+    red_x.place(x=random.randint(0,screen_height), y=random.randint(0,screen_width))
+    win.after(1000,update_position)
 
 
 if __name__ == "__main__":
-    root = tk.Tk()  # Main canvas
-    root.title("Simple Pop-up GUI")  # Labelling the main canvas
-    root.withdraw()  # Hide the main canvas
+    global win , red_x
+    win = tk.Tk()  # Main canvas
+    win.title("Simple Pop-up GUI")  # Labelling the main canvas
+    win.overrideredirect(1)  # Remove border
+    # Hide the main canvas
 
-    show_custom_popup()
-
-    root.mainloop()
-
-
+    # Set the position of the pop-up window
+    screen_width = win.winfo_screenwidth()
+    screen_height = win.winfo_screenheight()
+    win.geometry(f"{screen_width}x{screen_height}")
+    label = tk.Label(win, text="Click the red x to close")
+    label.pack(pady=20)
+    win.bind('<Motion>',callback)
+    red_x=tk.Button(win,text="X",bg="red", fg="white",width=2,height = 1,relief="flat",command=win.destroy)
+    red_x.pack()
+    update_position()
+    win.mainloop()
 
 
